94f5506f4c0970b09403097f2d9d677c870b406d
Josimario-SS <josimarioss121@gmail.com>
2025-01-10 18:28:57 -0300

    criei o script.js

---
+++ exercicios/arrays/script.js
@@ -0,0 +1,10 @@
+var item1 = "arroz"
+var item2 = "feijao"
+var item3 = "leite"
+var item4 = "macarrao"
+
+const lista = ["arroz", "feijao", "leite", "macarrao"]
+// console.log(lista[2])
+lista.push("carne"); // vai adicionar mais uma string/dado a nossa lista. 
+
+console.log(lista[lista.length - 1]); //mostra o último item da array.
