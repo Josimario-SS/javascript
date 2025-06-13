79ae81158085c526b174ff774a79a49e84d53adc
Josimário Silva <josimarioss121@gmail.com>
2025-06-12 22:55:34 -0300

    Fiz o exercício "Contador" em Javascript.

--- exercicios/EXJS/contador/script.js
+++ exercicios/EXJS/contador/script.js
@@ -1,44 +1,48 @@
-function verificar() {
-    let data = new Date();
-    let anoAtual = data.getFullYear();
-    let fano = document.getElementById('txtano');
-    let res = document.querySelector('div#res');
-    if (fano.value.length == 0 || Number(fano.value) > anoAtual) {
-        window.alert('[ERRO] Verifique os dados e tente novamente!');
+/* O contador irá ter que contar desde o número especificado na caixa "Início" e ir até o número que tiver na caixa "Fim" de acordo com o passo que for passado, ou seja, se INICIO == 0 E FIM == 10 E PASSO == 1, O CONTADOR == 1, 2, 3,... ATÉ O NÚMERO 10.*/ 
+
+
+
+function Contar(){
+    // Obtém os elementos do HTML pelos seus IDs
+    // e armazena os valores em variáveis
+    let inicio = document.getElementById('txtinicio');
+    let fim = document.getElementById('txtfim');
+    let passo = document.getElementById('txtpasso');
+    let contador = document.getElementById('contador');
+
+    
+    // Verifica se os campos estão preenchidos
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        alert('ERROR! Preencha todos os campos!');
+        contador.innerHTML = "Impossível contar!";
+        return;
     }
 
-    else {
-        let fsex = document.getElementsByName('radsex');
-        let idade = anoAtual - Number(fano.value);
-        let genero = '';
-        let img = document.createElement('img');
-        img.setAttribute('id', 'foto');
-
-        if (fsex[0].checked) {
-            genero = 'Homem';
-            if (idade >= 0 && idade < 10) {
-                img.setAttribute('src', 'img/img-garoto.png');
-            } else if (idade < 25) {
-                img.setAttribute('src', 'img/img_homem-jovem.png');
-            } else if (idade < 50) {
-                img.setAttribute('src', 'img/img_homem-midade.png');
-            } else {
-                img.setAttribute('src', 'img/img_homem-idoso.png');
-            }
-        } else if (fsex[1].checked) {
-            genero = 'Mulher';
-            if (idade >= 0 && idade < 10) {
-                img.setAttribute('src', 'img/img-garota.png');
-            } else if (idade < 25) {
-                img.setAttribute('src', 'img/mulher_jovem.png');
-            } else if (idade < 50) {
-                img.setAttribute('src', 'img/mulher_midade.png');
-            } else {
-                img.setAttribute('src', 'img/img_mulher-idosa.png');
-            }
-        }
-        
-        res.innerHTML = `Detectamos um(a) ${genero} com ${idade} anos.`;
-        res.appendChild(img);
+    // Verifica se o passo é maior que zero
+    if (Number(passo.value) <= 0) {
+        alert('ERROR! O passo deve ser maior que zero!');
+        contador.innerHTML = "Impossível contar!";
+        return;
     }
+
+    // Verifica se o início é maior que o fim
+    if (Number(inicio.value) > Number(fim.value)) {
+        alert('ERROR! O início não pode ser maior que o fim!');
+        contador.innerHTML = "Impossível contar!";
+        return;
+    }
+
+    // Se tudo estiver correto, faz a contagem
+    contador.innerHTML = '';
+    let ini = Number(inicio.value);
+    let f = Number(fim.value);
+    let p = Number(passo.value);
+
+    for (let c = ini; c <= f; c += p) {
+        contador.innerHTML += `${c} \u{1F449} `;
+    }
+    contador.innerHTML += `\u{1F3C1}`; // Adiciona o emoji de bandeira de chegada
+    
+
+    
 }
